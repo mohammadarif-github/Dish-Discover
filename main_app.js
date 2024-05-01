d079462d0cf9536f03c030b7e2f045cbd2e881f9
Mohammad Ariful Islam <mohammadarif.it159@gmail.com>
2024-05-01 23:08:50 +0600

    design updated

--- main_app.js
+++ main_app.js
@@ -1,28 +1,41 @@
 const loadrecipes = () => {
-    fetch("https://dish-discovery-backend.onrender.com/recipe/")
+  fetch("https://dish-discovery-backend.onrender.com/recipe/")
     .then((res) => res.json())
     .then((data) => display_recipes(data))
-    .catch((err)=>console.log(err))
-};
-
-const display_recipes = (recipes) => {
-    const parent = document.getElementById("recipe-container");
-    recipes.forEach(recipe => {
-        const recipecard = document.createElement("div");
-        recipecard.innerHTML = `
-            <div class="row">
-                <div class="single_recipe text-center col-md-6">
-                    <div class="recepie_thumb">
-                        <img src="${recipe.image}" alt="">
-                    </div>
-                    <h3>${recipe.name}</h3>
-                    <a href="description.html?recipe_id=${recipe.id}" class="line_btn" onclick="getparams()" >View Full Recipe</a>
-                </div>
-            </div>
-        `;
-        parent.appendChild(recipecard);
-    });
+    .catch((err) => console.log(err));
 };
 
 loadrecipes();
-                        
+
+const display_recipes = (recipes) => {
+    const gallerySection = document.getElementById("gallery");
+    const containerDiv = document.createElement("div");
+    containerDiv.classList.add("container");
+
+    let rowDiv; // Initialize rowDiv variable
+
+    recipes.forEach((recipe, index) => {
+        if (index % 3 === 0) { // Create a new row after every third recipe
+            rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
+            containerDiv.appendChild(rowDiv);
+        }
+    const colDiv = document.createElement("div");
+    colDiv.classList.add("col-lg-4", "mb-4");
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    cardDiv.innerHTML = `
+        <img src="${recipe.image}"alt="" class="card-img-top">
+        <div class="card-body">
+        <h5 class="card-title">${recipe.name}</h5>
+        <p class="card-text">${recipe.description.slice(0, 100)}</p>
+        <a href="description.html?id=${recipe.id}" class="btn btn-outline-success btn-sm">Full Recipe</a>
+        <a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+    </div> `;
+    colDiv.appendChild(cardDiv);
+    rowDiv.appendChild(colDiv);
+  });
+  containerDiv.appendChild(rowDiv);
+  gallerySection.appendChild(containerDiv);
+};
